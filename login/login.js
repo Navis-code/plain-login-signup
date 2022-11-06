86e4a8257fbfbe7f4c95a1deb576f15a3108922b
Navis-code <info@naviscode.com>
2022-11-06 10:10:36 +0100

    first commit

---
+++ login/login.js
@@ -0,0 +1,12 @@
+import '../style.css';
+
+const loginForm = document.querySelector('#loginForm');
+
+loginForm.addEventListener('submit', formSubmitted);
+
+function formSubmitted(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const formSerialized = Object.fromEntries(formData);
+  console.log(formSerialized);
+}
